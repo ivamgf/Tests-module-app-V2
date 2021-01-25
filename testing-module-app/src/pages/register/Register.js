504d1e29b5fa644f0c4a94c97ed2c5d6ded3b1ba
Ivam <ivam.developer@gmail.com>
2021-01-25 15:52:39 -0300

    New_screen-register

--- testing-module-app/src/pages/register/Register.js
+++ testing-module-app/src/pages/register/Register.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './register-style.css';
 import '../../App.css';
+import Form from'../../components/form/Form';
 
 export default class Register extends Component {
     render() {
         return (
             <article className="App-article">
                 <h3 className="styleTitle">Register</h3>                
-               
+                <Form />                
             </article>
         )
     }
